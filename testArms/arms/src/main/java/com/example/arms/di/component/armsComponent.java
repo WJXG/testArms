82c2a303392a688f9842ddb6073293e3b5e32e29
韦江星 <weijiangxing@bmsoft.com.cn>
2021-03-01 14:53:13 +0800

    注释报错文件

--- testArms/arms/src/main/java/com/example/arms/di/component/armsComponent.java
+++ testArms/arms/src/main/java/com/example/arms/di/component/armsComponent.java
@@ -1,41 +1,41 @@
-package com.example.arms.di.component;
-
-import dagger.BindsInstance;
-import dagger.Component;
-
-import com.jess.arms.di.component.AppComponent;
-
-import com.example.arms.di.module.armsModule;
-import com.example.arms.mvp.contract.armsContract;
-
-import com.jess.arms.di.scope.ActivityScope;
-import com.example.arms.mvp.ui.activity.armsActivity;
-
-
-/**
- * ================================================
- * Description:
- * <p>
- * Created by MVPArmsTemplate on 02/24/2021 10:27
- * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
- * <a href="https://github.com/JessYanCoding">Follow me</a>
- * <a href="https://github.com/JessYanCoding/MVPArms">Star me</a>
- * <a href="https://github.com/JessYanCoding/MVPArms/wiki">See me</a>
- * <a href="https://github.com/JessYanCoding/MVPArmsTemplate">模版请保持更新</a>
- * ================================================
- */
-@ActivityScope
-@Component(modules = armsModule.class, dependencies = AppComponent.class)
-public interface armsComponent {
-    void inject(armsActivity activity);
-
-    @Component.Builder
-    interface Builder {
-        @BindsInstance
-        armsComponent.Builder view(armsContract.View view);
-
-        armsComponent.Builder appComponent(AppComponent appComponent);
-
-        armsComponent build();
-    }
-}
+//package com.example.arms.di.component;
+//
+//import dagger.BindsInstance;
+//import dagger.Component;
+//
+//import com.jess.arms.di.component.AppComponent;
+//
+//import com.example.arms.di.module.armsModule;
+//import com.example.arms.mvp.contract.armsContract;
+//
+//import com.jess.arms.di.scope.ActivityScope;
+//import com.example.arms.mvp.ui.activity.armsActivity;
+//
+//
+///**
+// * ================================================
+// * Description:
+// * <p>
+// * Created by MVPArmsTemplate on 02/24/2021 10:27
+// * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+// * <a href="https://github.com/JessYanCoding">Follow me</a>
+// * <a href="https://github.com/JessYanCoding/MVPArms">Star me</a>
+// * <a href="https://github.com/JessYanCoding/MVPArms/wiki">See me</a>
+// * <a href="https://github.com/JessYanCoding/MVPArmsTemplate">模版请保持更新</a>
+// * ================================================
+// */
+//@ActivityScope
+//@Component(modules = armsModule.class, dependencies = AppComponent.class)
+//public interface armsComponent {
+//    void inject(armsActivity activity);
+//
+//    @Component.Builder
+//    interface Builder {
+//        @BindsInstance
+//        armsComponent.Builder view(armsContract.View view);
+//
+//        armsComponent.Builder appComponent(AppComponent appComponent);
+//
+//        armsComponent build();
+//    }
+//}
